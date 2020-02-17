90071e4e9078c1690632ffdadab9a0538c37ca80
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-02-17 17:48:48 -0600

    Adding attribution

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
-console.log("Hello World, my name is Rochelle Markham")
+/*=========================================================
+;Title: Excercise 1.3
+;Author: Professor Krasso
+;Date: 17 Feb 2020
+;Modified by: Rochelle Markham
+;Description: basic program running JavaScript application
+;in VS code, through Node's debugger console
+;==========================================================
+*/
+
+console.log("Hello World, my name is Rochelle Markham")  //output: hello world, my name is Rochelle Markham
