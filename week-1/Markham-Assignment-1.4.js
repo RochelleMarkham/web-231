1d080e77a5c8eebc453c09010b87aa4148bfe230
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-02-17 18:25:25 -0600

    Assignment 1.4: Primitive Types

---
+++ week-1/Markham-Assignment-1.4.js
@@ -0,0 +1,23 @@
+/*=======================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 17 Feb 2020
+; Modified by: Rochelle Markham
+; Description: basic program to display
+; personal details
+;========================================
+*/
+
+var firstName = "Rochelle";      //assigning personal details to variables
+var lastName = "Markham";
+var age = 34;
+var street = "1234 Easy Street";
+var city = "Anywhere, USA";
+var zipCode = 11235;
+
+console.log(firstName);         //console.log functions use to display each 
+console.log(lastName);          //variable on a new line
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
