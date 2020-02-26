419d4c15c8de448285e23a014a84d13256168747
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-02-26 12:41:46 -0600

    Adding Assignment 2.2

---
+++ Markham-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso 
+; Date:   26 February 2020
+; Modified by: Rochelle Markham
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./Markham-header.js');
+
+console.log(header.display("Rochelle", "Markham", "Assignment 2.2"));
